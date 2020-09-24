72d5f546671bd1b7dd8a80795477bace052101c1
Shirshendhu Panday <shirshendhupanday.97@gmail.com>
2020-09-24 18:01:56 +0530

    Create TestItems.java

---
+++ Exam1_24th Sept/src/com/demo/test/TestItems.java
@@ -0,0 +1,18 @@
+package com.demo.test;
+import com.demo.bean.Items;
+import com.demo.dao.ItemDaoImpl;
+import com.demo.dao.ItemDao;
+import com.demo.service.ItemService;
+import java.util.ArrayList;
+
+//Main Class to test
+
+public class TestItems {
+
+    public static void main(String[] args) {
+        ItemService itemService=new ItemService();
+        ArrayList<Items> items=new ArrayList<>();
+
+
+    }
+}
